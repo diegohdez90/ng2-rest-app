90371dc14d772c361f71b012c679580fea0d3849
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-17 17:26:19 -0500

    Update widgets.services to do CRUD task to widgets component

--- src/app/shared/widgets.service.ts
+++ src/app/shared/widgets.service.ts
@@ -4,34 +4,49 @@ import { Widget } from './widget.model';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
+const BASE_URL = 'http://localhost:3000/widgets/';
+const IMG_URL = 'assets/img/';
+const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
+
 @Injectable()
 export class WidgetsService {
 	selectedWidget: Widget;
-	widgets: Widget[] = [
-	{
-    'id': 1,
-    'img': 'assets/img/one.png',
-    'name': 'Widget 1',
-    'description': 'This is a description'
-  },
-  {
-    'id': 2,
-    'img': 'assets/img/two.png',
-    'name': 'Widget 2',
-    'description': 'This is a description!'
-  },
-  {
-    'id': 3,
-    'img': 'assets/img/three.png',
-    'name': 'Widget 3',
-    'description': 'This is a lovely widget'
-  }
-	];
-  constructor() {}
-
-  setSelectedWidget(w:Widget){
-  	this.selectedWidget = w;
+	widgets: Widget[] = [];
+  constructor(private http:Http) {}
+  loadWidgets(){
+    return this.http.get(`${BASE_URL}`)
+            .map(res => res.json())
+            .map(widgets => 
+                  widgets.map(
+                      widget => Object.assign(
+                        {}, 
+                        widget, 
+                        {
+                           img: `${IMG_URL}${widget.img}`
+                        }
+                       )
+                     )
+                 );
   }
 
-
+  saveWidget(widget){
+    return (widget.id) ? this.updateWidget(widget) : this.createWidget(widget);
   }
+
+  updateWidget(widget){
+    return this.http.patch(`${BASE_URL}${widget.id}`,JSON.stringify(widget),HEADER)
+                    .map(res =>  res.json())
+                    .map(i => Object.assign({},i,{img:`${IMG_URL}${i.img}`}));
+  }
+
+  createWidget(widget:Widget){
+    return this.http.post(`${BASE_URL}`,JSON.stringify(widget),HEADER)
+                    .map(res =>  res.json);
+  }
+
+  deleteWidget(widget){
+    return this.http.delete(`${BASE_URL}${widget.id}`)
+    .map(res => res.json());
+  }
+
+}
