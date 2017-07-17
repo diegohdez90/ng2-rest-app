9a61f1e3de3f81bb31017aaa472673b64a0c1ff1
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-17 17:24:03 -0500

    Update widgets component to display child components to list and detail widget

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -14,11 +14,48 @@ export class WidgetsComponent implements OnInit {
   constructor(private widgetsService : WidgetsService) { }
 
   ngOnInit() {
-  	this.widgets = this.widgetsService.widgets;
-  	console.log("Widgets Loaded");
+  	this.widgetsService.loadWidgets()
+    .subscribe(result => this.widgets = result);
   }
 
-	selectItem(widget){
+	selectWidget(widget){
 		this.selectedWidget = widget;
 	}
+
+  saveWidget(widget){
+    this.widgetsService.saveWidget(widget)
+                        .subscribe(responseWidget => {
+                          if(widget.id){
+                            this.replaceWidget(widget);
+                          }else{
+                            this.pushWidget(widget);
+                          }
+                          console.log(responseWidget);
+                        });
+    this.resetWidget();
+  }
+
+  pushWidget(widget:Widget){
+    this.widgets.push(widget);
+  }
+
+  replaceWidget(widget:Widget){
+    this.widgets = this.widgets.map(w => {
+      return w.id === widget.id ? widget : w;
+    })
+  }
+
+  deleteWidget(widget){
+    this.widgetsService.deleteWidget(widget)
+    .subscribe(() => 
+      this.widgets.splice(this.widgets.indexOf(widget),1)
+    );
+    
+  }
+
+
+  resetWidget(){
+    let emptyForm = { id:null, name:'', description:''};
+    this.selectedWidget=emptyForm;
+  }
 }
